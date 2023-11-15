d1d52c6336059effeb1e599cc9e3119a33aa67f3
pmpr.dev <pmpr.dev@gmail.com>
2023-11-15 11:45:02 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             654b452553879             |
+    |             6554af17a9c08             |
     |_______________________________________|
 */
  use Pmpr\Module\Security\Security; Security::symcgieuakksimmu();
--- src/Pmpr/Module/Security/Setting.php
+++ src/Pmpr/Module/Security/Setting.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             654b452553879             |
+    |             6554af17a9c08             |
     |_______________________________________|
 */
- namespace Pmpr\Module\Security; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Setting as BaseClass; class Setting extends BaseClass { public function qiccuiwooiquycsg() { $this->id = $this->akuociswqmoigkas(); parent::qiccuiwooiquycsg(); } public function gogaagekwoisaqgu() { $this->title = __("\x53\145\143\x75\162\x69\164\x79", PR__MDL__SECURITY); } }
+ namespace Pmpr\Module\Security; use Pmpr\Common\Foundation\FormGenerator\Backend\Setting\Setting as BaseClass; class Setting extends BaseClass { public function qiccuiwooiquycsg() { $this->id = $this->akuociswqmoigkas(); parent::qiccuiwooiquycsg(); } public function gogaagekwoisaqgu() { $this->title = __("\x53\x65\143\165\x72\x69\164\x79", PR__MDL__SECURITY); } }
